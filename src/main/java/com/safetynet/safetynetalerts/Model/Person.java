688d0beabc3ac1881ef0511b322ea7c444082df6
JohannaOCR <johanna.tristan.OCR@gmail.com>
2022-10-04 21:35:06 +0200

    Clean, ajout d'une sous-class PersonBuilder, modification de la fonction toString()

--- src/main/java/com/safetynet/safetynetalerts/Model/Person.java
+++ src/main/java/com/safetynet/safetynetalerts/Model/Person.java
@@ -1,107 +1,121 @@
 package com.safetynet.safetynetalerts.Model;
 
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
 public class Person {
-    public String firstName;
-    public String lastName;
-    public String phone;
-    public String zip;
-    public String address;
-    public String city;
-    public String email;
-    public Date birthdate;
-    public MedicalRecord medicalRecord;
+        public final String firstName;
+        public final String lastName;
+        public final String phone;
+        public final String zip;
+        public MedicalRecord medicalrecord;
+        public final String address;
+        public final String city;
+        public final String email;
 
 
-    public Person(String firstName, String lastName, String phone, String zip, String address, String city, String email) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.phone = phone;
-        this.zip = zip;
-        this.address = address;
-        this.city = city;
-        this.email = email;
-    }
+        public static class PersonBuilder {
+            private String firstName;
+            private String lastName;
+            private String phone;
+            private String zip;
+            private String address;
+            private String city;
+            private String email;
+            private MedicalRecord medicalrecord;
+
+            public PersonBuilder() {
+            }
+
+
+            public PersonBuilder firstName(String firstName) {
+                this.firstName = firstName;
+                return this;
+            }
+
+            public PersonBuilder lastName(String lastName) {
+                this.lastName = lastName;
+                return this;
+            }
+
+            public PersonBuilder phone(String phone) {
+                this.phone = phone;
+                return this;
+            }
+
+            public PersonBuilder zip(String zip) {
+                this.zip = zip;
+                return this;
+            }
+
+            public PersonBuilder address(String address) {
+                this.address = address;
+                return this;
+            }
+
+            public PersonBuilder city(String city) {
+                this.city = city;
+                return this;
+            }
+
+            public PersonBuilder email(String email) {
+                this.email = email;
+                return this;
+            }
+            public PersonBuilder firstName(MedicalRecord medicalRecord) {
+                this.firstName = firstName;
+                return this;
+            }
+
+            public Person build() {
+                return new Person(firstName, lastName, phone, zip, medicalrecord, address, city, email);
+            }
+        }
+
+        private Person(String firstName, String lastName, String phone, String zip, MedicalRecord medicalrecord, String address, String city, String email) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.phone = phone;
+            this.zip = zip;
+            this.medicalrecord = medicalrecord;
+            this.address = address;
+            this.city = city;
+            this.email = email;
+        }
 
     public String getFirstName() {
         return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
     }
 
     public String getLastName() {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
     public String getPhone() {
         return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
     }
 
     public String getZip() {
         return zip;
     }
 
-    public void setZip(String zip) {
-        this.zip = zip;
-    }
-
     public String getAddress() {
         return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
     }
 
     public String getCity() {
         return city;
     }
 
-    public void setCity(String city) {
-        this.city = city;
-    }
-
     public String getEmail() {
         return email;
     }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Date getBirthdate() {
-        return birthdate;
-    }
-
-    public void setBirthdate(Date birthdate) {
-        this.birthdate = birthdate;
-    }
-
-    public MedicalRecord getMedicalRecord() {
-        return medicalRecord;
-    }
-
-    public void setMedicalRecord(MedicalRecord medicalRecord) {
-        this.medicalRecord = medicalRecord;
-    }
-
     public Boolean isMajeur () {
         Date dateNow = new Date(System.currentTimeMillis());
         try {
-            Date Formatter = new SimpleDateFormat("dd/MM/yyyy", Locale.FRANCE).parse(String.valueOf(this.birthdate));
+            Date Formatter = new SimpleDateFormat("dd/MM/yyyy", Locale.FRANCE).parse(String.valueOf(this.medicalrecord.birthdate));
             long between = dateNow.getTime() - Formatter.getTime();
             if (between/(1000*60*60*24) > 6570 ){
                 return true;
@@ -112,6 +126,25 @@ public class Person {
         }
         return null;
     }
+
+    @Override
+    public String toString() {
+        return "\n" + "Person {"
+                + "\n" +"firstName = '" + firstName + '\'' + "\n"+
+                "lastName = '" + lastName + '\'' + "\n"+
+                "phone = '" + phone + '\'' + "\n"+
+                "zip = '" + zip + '\'' + "\n"+
+                "medicalrecord ==> " + medicalrecord.toString() + "\n"+
+                "address = '" + address + '\'' + "\n"+
+                "city = '" + city + '\'' + "\n"+
+                "email = '" + email + '\'' + "\n"+
+                '}' + "\n" ;
+    }
 }
+
+
+
+
+
 
 
