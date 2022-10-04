95aa17912c4071120eb4b0fd9041373c332144f7
JohannaOCR <johanna.tristan.OCR@gmail.com>
2022-10-04 22:13:15 +0200

    Suppression du SpringBootTest et ajout Junit

--- src/test/java/com/safetynet/safetynetalerts/SafetyNetAlertsApplicationTests.java
+++ src/test/java/com/safetynet/safetynetalerts/SafetyNetAlertsApplicationTests.java
@@ -1,9 +1,9 @@
 package com.safetynet.safetynetalerts;
+import org.junit.Test;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class SafetyNetAlertsApplicationTests {
 
     @Test
